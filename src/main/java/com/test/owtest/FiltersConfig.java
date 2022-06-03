7bfe1903503412ea3195561c31b2c00814c3adaf
Maxim Grigorov <mgrigorov@openwaygroup.com>
2022-06-03 17:05:34 +0300

    Initial commit

---
+++ src/main/java/com/test/owtest/FiltersConfig.java
@@ -0,0 +1,16 @@
+package com.test.owtest;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+@Configuration
+public class FiltersConfig {
+    @Bean
+    public CharacterEncodingFilter characterEncodingFilter() {
+        final CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        return characterEncodingFilter;
+    }
+}
